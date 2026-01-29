2c6da59e5a9b9991d8336d2a6c6d597a3516517d
Bright Chen <chenguangmingfe@foxmail.com>
2026-01-29 10:13:08 +0800

    Fix some issues with transport (#3206)
    1. The return value of CreateTransport should be std::unique_ptr.
    2. Delete BAIDU_REGISTER_ERRNO in transport_factory.h.
    3. Optimize some code formatting.

--- src/brpc/input_messenger.cpp
+++ src/brpc/input_messenger.cpp
@@ -382,7 +382,8 @@ void InputMessenger::OnNewMessages(Socket* m) {
             }
         }
 
-        if (messenger->ProcessNewMessage(m, nr, read_eof, received_us, base_realtime, last_msg) < 0) {
+        if (messenger->ProcessNewMessage(m, nr, read_eof, received_us,
+                                         base_realtime, last_msg) < 0) {
             return;
         }
     }
