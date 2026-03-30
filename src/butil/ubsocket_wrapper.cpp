078d11c290270eb878dd3adc6b1cd713a39c0377
Zhiwei Chen <chenzhiwei68@huawei.com>
2026-03-30 22:37:43 +0800

    Use ubsocket_enable
    Mention ubsocket_enable in docs

--- src/butil/ubsocket_wrapper.cpp
+++ src/butil/ubsocket_wrapper.cpp
@@ -32,10 +32,10 @@
 
 #define DELEGATE(f)                                         \
     ({                                                      \
-        FLAGS_ubsocket_enable_wrapper ? UB_API_WRAP(f) : f; \
+        FLAGS_ubsocket_enable ? UB_API_WRAP(f) : f; \
     })
 
-DEFINE_bool(ubsocket_enable_wrapper, true, "Enable the ubsocket interposition layer. If true, socket calls are routed through ubsocket; otherwise, the original socket api is used.");
+DEFINE_bool(ubsocket_enable, true, "Enable the ubsocket interposition layer. If true, socket calls are routed through ubsocket; otherwise, the original socket api is used.");
 
 extern "C" {
 int ubsocket_wrapper_socket(int domain, int type, int protocol) {
