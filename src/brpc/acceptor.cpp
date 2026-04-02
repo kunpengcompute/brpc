9476dd76e42a1aeb66c24f2ccab213c276483916
gcw_GyK5eQlb <liushunbi@h-partners.com>
2026-04-02 20:18:05 +0800

    clean code

--- src/brpc/acceptor.cpp
+++ src/brpc/acceptor.cpp
@@ -335,12 +335,12 @@ void Acceptor::OnNewConnectionsUntilEAGAIN(Socket* acception) {
 }
 
 void Acceptor::OnNewConnections(Socket* acception) {
-    int progress = Socket::PROGRESS_INIT;	 
-    do {	 
-        OnNewConnectionsUntilEAGAIN(acception);	 
-        if (acception->Failed()) {	 
-            return;	 
-        }	 
+    int progress = Socket::PROGRESS_INIT;
+    do {
+        OnNewConnectionsUntilEAGAIN(acception);
+        if (acception->Failed()) {
+            return;
+        }
     } while (acception->MoreReadEvents(&progress));
 }
 
